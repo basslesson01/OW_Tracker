d5787279c5b2e721cbc3f276315a0ff35cc57eef
basslesson01 <lkim@unb.ca>
2016-12-29 20:16:00 -0400

    Updated CSS, JS and HTML file

--- js/controller.js
+++ js/controller.js
@@ -1,12 +1,29 @@
 angular.module('OW_Trakcer_App',[])
-  .controller('myController',['$scope','$http',function($scope,$http) {
-    $scope.testing = function(battletag){
+  .controller('myController',['$scope','$http','myFactory',function($scope,$http,myFactory) {
+    $scope.getProfile = function(battletag){
       var btag = document.getElementById('battletag').value;
       console.log(btag);
       $http.get('https://api.lootbox.eu/pc/us/' + btag + '/profile').then(function(data){
-        console.log('Battletag is: ' + data.data.data.username);
+        myFactory.setBattleTag(btag);
+        console.log('Battletag is: ' + myFactory.getBattleTag());
         console.log('Level is: ' + data.data.data.level);
+        console.log('Quickplay Wins: ' + data.data.data.games.quick.wins); //Wow. Such nest. Much crap
+        console.log('Skill rating: ' + data.data.data.competitive.rank);
       });
-    }//End of testing
+    }//End of getProfile
 
   }])
+
+  .factory('myFactory',function(){
+    var battletag = {
+      tag : ''
+    };
+    return {
+      getBattleTag: function() {
+        return battletag.tag;
+      },
+      setBattleTag: function(Tag){
+        battletag.tag = Tag;
+      }
+    };
+  });
