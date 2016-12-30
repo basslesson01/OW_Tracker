dadae60945510c2407241ccec517b5883b864e96
basslesson01 <lkim@unb.ca>
2016-12-29 21:41:47 -0400

    More changes

--- js/controller.js
+++ js/controller.js
@@ -1,22 +1,43 @@
 angular.module('OW_Trakcer_App',[])
   .controller('myController',['$scope','$http','myFactory',function($scope,$http,myFactory) {
-    $scope.getProfile = function(battletag){
+    $scope.setProfile = function(battletag){
       var btag = document.getElementById('battletag').value;
+      myFactory.setBattleTag(btag);
+      $http.get('https://api.lootbox.eu/pc/us/' + btag + '/profile').then(function(data){
+        myFactory.setProfile(data.data.data.level, data.data.data.games.quick.wins, data.data.data.games.competitive.wins, data.data.data.games.competitive.played, data.data.data.competitive.rank);
+      });
+    } //End of setProfile
+
+    $scope.checkSess = function(){
+      var btag = myFactory.getBattleTag();
+      return btag;
+    }
+
+    $scope.getProfile = function(){
+      var btag = myFactory.getBattleTag();
       console.log(btag);
       $http.get('https://api.lootbox.eu/pc/us/' + btag + '/profile').then(function(data){
-        myFactory.setBattleTag(btag);
-        console.log('Battletag is: ' + myFactory.getBattleTag());
-        console.log('Level is: ' + data.data.data.level);
-        console.log('Quickplay Wins: ' + data.data.data.games.quick.wins); //Wow. Such nest. Much crap
-        console.log('Skill rating: ' + data.data.data.competitive.rank);
+        console.log('Battletag is: ' + btag);
+        var profilelevel = myFactory.getProfile().level;
+        var profileQpWins = myFactory.getProfile().qpWins;
+        var profileCpWins = myFactory.getProfile().cpWins;
+        var profileCpPlayed = myFactory.getProfile().cpPlayed;
+        var profileCpRating = myFactory.getProfile().cpRating;
+        return myFactory.getProfile();
       });
-    }//End of getProfile
-
+    } //End of getProfile
   }])
 
   .factory('myFactory',function(){
     var battletag = {
       tag : ''
+    };
+    var profile = {
+      level : '',
+      qpWins : '',
+      cpWins : '',
+      cpPlayed : '',
+      cpRating : ''
     };
     return {
       getBattleTag: function() {
@@ -24,6 +45,16 @@ angular.module('OW_Trakcer_App',[])
       },
       setBattleTag: function(Tag){
         battletag.tag = Tag;
+      },
+      setProfile: function(lvl,qWin,cWin,cPlayed,cRating){
+        profile.level = lvl;
+        profile.qpWins = qWin;
+        profile.cpWins = cWin;
+        profile.cpPlayed = cPlayed;
+        profile.cpRating = cRating;
+      },
+      getProfile: function(){
+        return profile;
       }
     };
   });
